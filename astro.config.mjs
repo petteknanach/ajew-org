db9d6aa96601bbfb02c81bbf5c474381746785dc
Pettek <petteknanach@gmail.com>
2026-02-13 05:54:50 +0200

    Fix: change output hybrid to static for Astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ajew.org',
-  output: 'hybrid',
+  output: 'static',
   vite: {
     css: {
       minify: false
